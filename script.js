f432360123960e5f9c85256d2f687afbe8284853
Pradeep Kumar <95734296+Impradeep2000@users.noreply.github.com>
2023-08-22 18:47:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let a = document.getElementById("colorSelect");
+let b = document.getElementByTagName("input");
+
+b.addEventListener("click" , () => {
+	let deleteOp = select.options[select.selectedIndex];
+
+	if(deleteOp){
+		deleteOp.remove();
+	}
+});
