0fe4169ace24599124c0cd94e81f9b27139d142b
Pradeep Kumar <95734296+Impradeep2000@users.noreply.github.com>
2023-08-22 18:55:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var button = document.getElementById("removeButton");
 
 button.addEventListener("click", function () {
-    var select = document.getElementById("colorSelect");
+    var select = document.getElementsById("colorSelect");
     var selectedOption = select.querySelector("option:checked");
 
     if (selectedOption) {
