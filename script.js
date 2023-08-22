03483731e34edc1e4ea875605184e3b0eb448339
Pradeep Kumar <95734296+Impradeep2000@users.noreply.github.com>
2023-08-22 19:02:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 //your JS code here. If required.
 let a = document.getElementById("colorSelect");
-let b = document.getElementsByTagName("input");
-
-b.addEventListener("click" , () => {
-	let deleteOp = select.options[select.selectedIndex];
-
-	if(deleteOp){
-		deleteOp.remove();
+let b = document.getElementsByTagName("input")[0];
+b.addEventListener("click" , () =>{
+	let option = document.getElementByTagName("option");
+	for(let i=0;i<option.length;i++){
+		if(option[i].innerText === a.value){
+			return a.removeChild(option[i]);
+		}
 	}
 });
+
