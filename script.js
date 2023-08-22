b95efe9c328503d7a7f640e5b863ffe8419c5c63
Pradeep Kumar <95734296+Impradeep2000@users.noreply.github.com>
2023-08-22 19:06:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //your JS code here. If required.
-let a = document.getElementById("colorSelect");
+let a = document.querySelector("#colorSelect");
 let b = document.getElementsByTagName("input")[0];
 b.addEventListener("click" , () =>{
-	let option = document.getElementByTagName("option");
+	let option = document.getElementsByTagName("option");
 	for(let i=0;i<option.length;i++){
 		if(option[i].innerText === a.value){
 			return a.removeChild(option[i]);
